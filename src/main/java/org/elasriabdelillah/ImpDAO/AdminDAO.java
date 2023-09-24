750a81ad5ae42b1ec6a9b9ac718d0878515ae0a4
MHkif <malikhkif@gmail.com>
2023-09-25 01:34:53 +0200

    Create Admin Auth

--- src/main/java/org/elasriabdelillah/ImpDAO/AdminDAO.java
+++ src/main/java/org/elasriabdelillah/ImpDAO/AdminDAO.java
@@ -22,7 +22,7 @@ public class AdminDAO {
             preparedStatement.setString(1, email);
             preparedStatement.setString(2, password);
             ResultSet res = preparedStatement.executeQuery();
-            while (res.next()){
+            if (res.next()){
                 admin = Admin.getInstance(
                         res.getString("id"), res.getString("fullName"),
                         res.getString("email"), res.getString("password") );
--- src/main/java/org/elasriabdelillah/Main.java
+++ src/main/java/org/elasriabdelillah/Main.java
@@ -1,7 +1,11 @@
 package org.elasriabdelillah;
 
+import org.elasriabdelillah.ImpDAO.AdminDAO;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+        
     }
 }
